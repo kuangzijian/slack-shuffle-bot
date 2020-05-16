0a3329722b8cc5e476e7f73a89173276c8c74315
Jack Kuang <kuangzijian1@hotmail.com>
2020-05-15 22:43:28 -0600

    Added some to-do items

--- ShuffleBot/Controllers/API/ShuffleAPIController.cs
+++ ShuffleBot/Controllers/API/ShuffleAPIController.cs
@@ -11,12 +11,15 @@ namespace ShuffleBot.Controllers.API
     {
         public IHttpActionResult Shuffle()
         {
+            // to-do: need to move the configuration to a secrete configuration file.
             var WebHookUrl = "https://hooks.slack.com/services/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";
 
             var client = new SbmClient(WebHookUrl);
 
+            // to-do: need to figure out the format of the payload send from slack command.
             var shuffleList = ("Jack,Ryan,Tin,Josh").Split(',').ToList().OrderBy(a => Guid.NewGuid()).ToList();
 
+            // to-do: need to figure out if there is a way to integrate with attandance bot and grab the day off information from there.
             var dayOffList = "Mike, Rui";
             var reminder = "@channel Please do not forget to update your remaining points on the card that you are working on.";
 
