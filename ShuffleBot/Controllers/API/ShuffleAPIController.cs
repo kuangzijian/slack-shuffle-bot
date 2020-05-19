58c7cfdb22b05f52287c8ba80ea7a989d38987fb
Jack Kuang <kuangzijian1@hotmail.com>
2020-05-19 08:45:13 -0600

    Moved varibles to web.config

--- ShuffleBot/Controllers/API/ShuffleAPIController.cs
+++ ShuffleBot/Controllers/API/ShuffleAPIController.cs
@@ -4,6 +4,7 @@ using System.Web.Http;
 using SlackBotMessages.Models;
 using System.Linq;
 using System;
+using System.Web.Configuration;
 
 namespace ShuffleBot.Controllers.API
 {
@@ -11,17 +12,17 @@ namespace ShuffleBot.Controllers.API
     {
         public IHttpActionResult Shuffle()
         {
-            // to-do: need to move the configuration to a secrete configuration file.
-            var WebHookUrl = "https://hooks.slack.com/services/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";
-
+            var WebHookUrl =  WebConfigurationManager.AppSettings["WebHookUrlTest"];
+            
             var client = new SbmClient(WebHookUrl);
 
-            // to-do: need to figure out the format of the payload send from slack command.
-            var shuffleList = ("Jack,Ryan,Tin,Josh").Split(',').ToList().OrderBy(a => Guid.NewGuid()).ToList();
+            var staffList = WebConfigurationManager.AppSettings["StaffList"];
+
+            var shuffleList = staffList.Split(',').ToList().OrderBy(a => Guid.NewGuid()).ToList();
 
             // to-do: need to figure out if there is a way to integrate with attandance bot and grab the day off information from there.
-            var dayOffList = "Mike, Rui";
-            var reminder = "@channel Please do not forget to update your remaining points on the card that you are working on.";
+            var dayOffList = "";
+            var reminder = "@channel Please don't forget to update your remaining points on the card that you are working on.";
 
 			var message = new Message(System.DateTime.Now.Date.ToString("MMMM dd"));
             message.AddAttachment(new Attachment()
