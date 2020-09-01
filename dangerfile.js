cd9a6f6536e7fe27087148617722df5fc9268199
Zijian Kuang <kuangzijian1@hotmail.com>
2020-09-01 10:10:19 -0600

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -8,10 +8,6 @@ if (!(danger.github.pr.body.includes('Type of Change'))) {
   fail('Please specify the type of change in the Pull Request description 💩')
 }
 
-if (!(danger.github.pr.body.includes('Type of Change'))) {
-  fail('Please specify the type of change in the Pull Request description 💩')
-}
-
 if (!(danger.github.pr.body.includes('Solution'))) {
   fail('Please specify the solution in the Pull Request description 💩')
 }
