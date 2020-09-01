3c47b260582df2c697d8251626eb814beb451b48
Zijian Kuang <kuangzijian1@hotmail.com>
2020-09-01 11:20:49 -0600

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,14 @@
-if (!(danger.github.pr.body.includes('AB#'))) {
+const prBody = danger.github.pr.body
+const adoLink_regex = /(AB#)[0-9]+/
+      
+if ((prBody.match(adoLink_regex)===null)) {
   fail('Please provide a link to the relevant card in the Pull Request description 💩')
 }
 
-if (!(danger.github.pr.body.includes('Type of Change'))) {
+if (!(prBody.includes('Type of Change'))) {
   fail('Please specify the type of change in the Pull Request description 💩')
 }
 
-if (!(danger.github.pr.body.includes('Solution'))) {
+if (!(prBody.includes('Solution'))) {
   fail('Please specify the solution in the Pull Request description 💩')
 }
